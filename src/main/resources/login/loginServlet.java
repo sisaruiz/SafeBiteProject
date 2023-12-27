caa3a4c05ba32150f962ca16c4482d489eedafdc
Sisa Ruiz <sisaruizm@gmx.com>
2023-12-27 20:45:08 +0100

    upload maven  project

---
+++ src/main/resources/login/loginServlet.java
@@ -0,0 +1,33 @@
+package resources.login;
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        DB database = mongoClient.getDB("SafeBite");
+        DBCollection users = database.getCollection("Users");
+
+        DBObject userObject = users.findOne(new BasicDBObject("user_name", username));
+
+        if (userObject == null) {
+            response.sendRedirect("login.jsp?error=invalid_credentials");
+            return;
+        }
+
+        if (!password.equals(userObject.getString("password"))) {
+            response.sendRedirect("login.jsp?error=invalid_credentials");
+            return;
+        }
+
+        session.setAttribute("username", username);
+        response.sendRedirect("home.jsp");
+    }
+}
