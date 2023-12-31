9bf92865df657054a3b8782221a0f2727d9274e9
Sisa Ruiz <sisaruizm@gmx.com>
2023-12-31 09:47:18 +0100

    update

--- src/main/resources/login/loginServlet.java
+++ src/main/resources/login/loginServlet.java
@@ -1,5 +1,5 @@
 package resources.login;
-@WebServlet("/login")
+@WebServlet("/loginServlet")
 public class LoginServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
