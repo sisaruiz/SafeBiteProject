964633f9eeb5eb23c9258517470a0c5079fbba11
Sisa Ruiz <sisaruizm@gmx.com>
2024-01-12 11:05:58 +0100

    connect to mongoDB users dataset

--- src/main/java/login/LoginServlet.java
+++ src/main/java/login/LoginServlet.java
@@ -11,6 +11,11 @@ import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import org.bson.Document;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.client.*;
+
 /**
  * Servlet implementation class LoginServlet
  */
@@ -21,24 +26,35 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		String n = request.getParameter("t1");
 		String pwd = request.getParameter("t2");
 		PrintWriter out = response.getWriter();
 		response.setContentType("text/html");
 		
+		//Create connection string
+		ConnectionString uri = new ConnectionString("mongodb://localhost:27017");
+		//Create a mongoDB client
+		MongoClient myClient = MongoClients.create(uri);
+		//Connect to SafeBite database
+		MongoDatabase database = myClient.getDatabase("SafeBite");
+		//Select the collection test
+		MongoCollection<Document> usersCollection = database.getCollection("Users");
 		
-		if(n.equals("sisa") && pwd.equals("sisapaxi")) {
-			HttpSession hs=request.getSession();
+        // Perform authentication logic
+        Document user = usersCollection.find(new Document("user_name", n).append("password", pwd)).first();
+
+        if (user != null) {
+            // Authentication successful
+        	HttpSession hs=request.getSession();
 			hs.setAttribute("uname", n);
 			response.sendRedirect("success.jsp");
-			
-		}
-		else {
-			out.println("<font color=red size=14 face=verdana>Sorry! username or password incorrect ... Try again!");
+        } else {
+            // Authentication failed
+        	out.println("<font color=red size=14 face=verdana>Sorry! username or password incorrect ... Try again!");
 			RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
 			rd.include(request, response);
-		}
-		
+        }
 		
 	}
 
